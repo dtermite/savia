82d84631e133ea6cd1d48e21302ffd6aac8c9d7f
Tu nombre aquíDiego <diegotermite@gmail.com>
2022-02-03 16:07:58 -0300

    se dividio index y se agrego encabezado y pie

--- index.php
+++ index.php
@@ -1,26 +1,7 @@
-<!doctype html>
-<html lang="es">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/estilos.css">
+          <!--Encabezados-->
+          <?php require_once 'encabezado.php';?>
 
-    <title>Savia Tierra Encuesta</title>
-  </head>
-  <body>
-      <div class="container">
-
-        <!--Encabezado-->
-        <header class="row mt-3">
-          <div class="col-12">
-            <img src="imagenes/LogoSaviaTierra.jpg" class="rounded mx-auto d-block" alt="...">
-          </div>
-        </header>
-        <main>
           <!--Introduccion-->
           <?php require_once 'introduccion.php';?>
 
@@ -153,16 +134,7 @@
             include("respuestas.php");
           ?>
 
-        </main>
+          <!--Pie de Pagina-->
+          <?php require_once 'pie.php' ?>
 
-        <footer>
-
-        </footer>
-          
-          
-      </div>
-    
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-  </body>
-</html>
+  
